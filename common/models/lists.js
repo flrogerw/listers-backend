dc17b492efdccc0cfb33ff2ef9e3e70da93cf162
Roger Williams <roger@photoandgo.com>
2016-06-27 13:57:42 -0600

    asd

--- common/models/lists.js
+++ common/models/lists.js
@@ -9,6 +9,8 @@ Lists.getAllByCategory = function(req, cb){
 	
 	var Mongo = app.models.Mongo;
 	
+	console.log(req.body);
+	
 	Mongo.getAll(req.params.category, req.body, function(err, response){
 		
 	
