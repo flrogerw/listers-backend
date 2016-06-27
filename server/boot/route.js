3ad286ee60e414dd3d3b31880c479b5895d6a755
Roger Williams <roger@photoandgo.com>
2016-06-26 22:33:34 -0600

    Adding Lists

--- server/boot/route.js
+++ server/boot/route.js
@@ -7,6 +7,7 @@
 module.exports = function(app) {
 	
 	var Token = app.models.Token;
+	var Lists = app.models.Lists;
 	/***** TOKEN ********/
 	
 	app.all(['/users','/users/*'], function(req, res, next){
