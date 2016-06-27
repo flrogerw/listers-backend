24a44dd60ff546df223e0f1b22bf7c6515ca6eac
Roger Williams <roger@photoandgo.com>
2016-06-27 00:32:31 -0600

    eertr

--- common/models/mongo.js
+++ common/models/mongo.js
@@ -3,31 +3,29 @@ var MongoClient = require('mongodb').MongoClient;
 var ObjectID = require('mongodb').ObjectID;
 
 module.exports = function(Mongo) {
-	
-	
-	
-Mongo.getAllInCategory = function(listType, cb){
-	
-	MongoClient.connect("mongodb://localhost:27017/Lists", function(err, db) {
-		if (!err) {
-			
-			
-			var collection = db.collection(listType);
-			collection.findOne({},function(err, response) {
-				
-				if(err){
-					cb(true, err);
-				}else{
-					cb(false, response);
-				}
-				db.close();
-			});
-		}else{
-			console.log("MONGO ERROR: "+err);
-			cb(true, err);
-		}
-	});	
-};	
-	
+
+	Mongo.getAllInCategory = function(listType, cb) {
+
+		MongoClient.connect("mongodb://localhost:27017/Lists",
+				function(err, db) {
+
+					if (!err) {
+
+						var collection = db.collection(listType);
+						collection.find({}, function(err, response) {
+
+							if (err) {
+								cb(true, err);
+							} else {
+								cb(false, response);
+							}
+							db.close();
+						});
+					} else {
+						console.log("MONGO ERROR: " + err);
+						cb(true, err);
+					}
+				});
+	};
 
 }
