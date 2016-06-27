0180858e8b44c08c8b82766de181beb12c3e7472
Roger Williams <roger@photoandgo.com>
2016-06-27 14:04:33 -0600

    ewrr

--- common/models/lists.js
+++ common/models/lists.js
@@ -1,5 +1,5 @@
 var app = require("../../server/server");
-
+var Mongo = app.models.Mongo;
 
 module.exports = function(Lists) {
 	
@@ -7,14 +7,10 @@ module.exports = function(Lists) {
 	
 Lists.getAllByCategory = function(req, cb){
 	
-	var Mongo = app.models.Mongo;
 	
-	console.log(req.body);
 	
 	Mongo.getAll(req.params.category, req.body, function(err, response){
-		
-	
-		
+				
 		cb({"error":err, "response": response});
 	});
 	
